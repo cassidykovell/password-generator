1fef1adbf7669495b28d6764bc492a9a7f3c027d
Cassidy <cassidy0kovell@gmail.com>
2024-01-11 09:10:48 -0800

    updated the main function, set conditions, added global variables, and got the website to generate a password

--- Develop/script.js
+++ Develop/script.js
@@ -86,64 +86,59 @@ var specialGlobal = ['@',
 '.',]
 
 function generatePassword() {
-  var userInput = prompt("How many characters would you like your pasword to have?:");
-  userInput = parseInt(userInput); 
+  var userInput = prompt("How many characters would you like your password to have?");
+  userInput = parseInt(userInput);
 
   if (isNaN(userInput) || userInput < 8 || userInput > 128) {
     alert("Please choose a number between 8 and 128");
-    return generatePassword()
+    return generatePassword();
   }
 
-var lowerCase = confirm("Do you want to use lower case letters?");
-console.log(lowerCase)
+  var lowerCase = confirm("Do you want to use lowercase letters?");
+  console.log(lowerCase);
 
-var upperCase = confirm("Do you want to use uppercase letters?");
-console.log(upperCase)
+  var upperCase = confirm("Do you want to use uppercase letters?");
+  console.log(upperCase);
 
-var numbers = confirm("Do you want to use numbers?");
-console.log(numbers)
+  var numbers = confirm("Do you want to use numbers?");
+  console.log(numbers);
 
-var special = confirm("Do you want to use special characers?");
-console.log(special)
+  var special = confirm("Do you want to use special characters?");
+  console.log(special);
 
-var possibleCharaters = [] 
+  var possibleCharacters = [];
 
-if (lowerCase===true) {
- possibleCharaters = possibleCharaters.concat(lowercaseGlobal);
- console.log(possibleCharaters)
+  if (lowerCase === true) {
+    possibleCharacters = possibleCharacters.concat(lowercaseGlobal);
+    console.log(possibleCharacters);
+  }
+
+  if (upperCase === true) {
+    possibleCharacters = possibleCharacters.concat(uppercaseGlobal);
+    console.log(possibleCharacters);
+  }
+
+  if (numbers === true) {
+    possibleCharacters = possibleCharacters.concat(numbersGlobal);
+    console.log(possibleCharacters);
+  }
+
+  if (special === true) {
+    possibleCharacters = possibleCharacters.concat(specialGlobal);
+    console.log(possibleCharacters);
+  } else if (lowerCase === false && upperCase === false && numbers === false && special === false) {
+    alert("You have to choose one of the parameters: lowercase, uppercase, numbers, or special characters!");
+    return generatePassword();
+  }
+
+  var password = "";
+
+  for (var i = 0; i < userInput; i++) {
+    password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+  }
+
+  return password;
 }
-
-if (upperCase===true) {
-  possibleCharaters = possibleCharaters.concat(uppercaseGlobal);
-  console.log(possibleCharaters)
- }
-
- if (numbers===true) {
-  possibleCharaters = possibleCharaters.concat(numbersGlobal);
-  console.log(possibleCharaters)
- }
-
- if (special===true) {
-  possibleCharaters = possibleCharaters.concat(specialGlobal);
-  console.log(possibleCharaters)
- }
-
- else if (lowerCase===false && upperCase===false && numbers===false && special && false) {
-  alert("You have to choose one of the perameters either, lowercase, uppercase, numbers, or special characters!");
-  return generatePassword()
- } 
-
- else {
-  generatePassword()
- }
-
-  for (i = 0; i < user.userInput.length; i++) {
-    (possibleCharaters[Math.floor(Math.random() * possibleCharaters.length)]);
-  }
-};
-
-
-// if user wants lowercase letters I have to add that to my array possible characters, if user wants upper case letter I have to add that to my array of possible characters, if my user want numbers then I add that 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
